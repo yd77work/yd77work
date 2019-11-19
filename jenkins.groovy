9eadab114cc297071a40b76f32205b3d73937e16
Yaroslav Duma <56842319+yd77work@users.noreply.github.com>
2019-11-19 20:10:30 +0200

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,33 +1,23 @@
-properties([
-    pipelineTriggers([
-        GenericTrigger(
-            causeString: 'Push to master', 
-            genericVariables: [[
-                defaultValue: '',
-                key: 'ref', 
-                regexpFilter: '', 
-                value: '$.ref'
-            ]], 
-            printContributedVariables: true, 
-            printPostContent: true, 
-            regexpFilterExpression: 'master$', 
-            regexpFilterText: '$ref', 
-            silentResponse: true, 
-            token: '71B6B68DFC8C34235B642BE2'
-        )
-    ])
-])
-
-node(){
-    stage(){
-          sshagent(['uuuuuuuuuuuuu']) {
-            sh "git clone -b master git@github.com:/tests.git ."
-        }
-        
-http://35.223.111.195/generic-webhook-trigger/invoke?token=71B6B68DFC8C34235B64    }
-    
-}      
-input("Please approved deploy to.")
-node(){
-    
+properties([pipelineTriggers([cron('*/5 * * * *')])])
+node {
+    stage('stage01') 
+    {
+        sh"uptime"
+        deleteDir()
+     }
+    stage("github")
+    {
+    withCredentials([usernamePassword(credentialsId: '9c3062a2-17a2-4617-a332-6cff68b12cea', passwordVariable: 'password', usernameVariable: 'username')]) 
+    {sh"git clone https://$username:$password@github.com/yd77work/Ansible"
+    }
+    }
+    stage("stage02")
+    {
+        def workDir = sh(returnStdout: true, script: "pwd").trim()
+        sh "cd $workDir && cd ./Ansible && ls -lh"
+    }
+    stage("stage03")
+    {
+        archiveArtifacts artifacts: "https://github.com/yd77work/Ansible/tree/master/ansible/*.yml", fingerprint: true
+     }
 }
