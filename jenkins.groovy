aec1e48e3e8166656fda4a1759fe98f9a5571b90
Yaroslav Duma <56842319+yd77work@users.noreply.github.com>
2019-11-19 20:16:40 +0200

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,4 +1,24 @@
-properties([pipelineTriggers([cron('*/5 * * * *')])])
+//properties([pipelineTriggers([cron('*/5 * * * *')])])
+properties([
+    pipelineTriggers([
+        GenericTrigger(
+            causeString: 'Push to master', 
+            genericVariables: [[
+                defaultValue: '',
+                key: 'ref', 
+                regexpFilter: '', 
+                value: '$.ref'
+            ]], 
+            printContributedVariables: true, 
+            printPostContent: true, 
+            regexpFilterExpression: 'master$', 
+            regexpFilterText: '$ref', 
+            silentResponse: true, 
+            token: '32REFERGTG544GTOGK4TO5GT'
+        )
+    ])
+])
+
 node {
     stage('stage01') 
     {
@@ -7,8 +27,9 @@ node {
      }
     stage("github")
     {
-    withCredentials([usernamePassword(credentialsId: '9c3062a2-17a2-4617-a332-6cff68b12cea', passwordVariable: 'password', usernameVariable: 'username')]) 
-    {sh"git clone https://$username:$password@github.com/yd77work/Ansible"
+    http://35.223.111.195/generic-webhook-trigger/invoke?token=32REFERGTG544GTOGK4TO5GT 
+    //withCredentials([usernamePassword(credentialsId: '9c3062a2-17a2-4617-a332-6cff68b12cea', passwordVariable: 'password', usernameVariable: 'username')]) 
+    //{sh"git clone https://$username:$password@github.com/yd77work/Ansible"
     }
     }
     stage("stage02")
